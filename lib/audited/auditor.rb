c25125f778801e13ad7576f6ead0cf21db36d93b
Justin Kronz <jkronz@gmail.com>
2015-11-25 12:00:15 -0500

    Remove audit#version
    - remove calls to set the version number
    - update scopes that rely on version number to find ancestor versions.

--- lib/audited/auditor.rb
+++ lib/audited/auditor.rb
@@ -51,7 +51,7 @@ module Audited
 
         attr_accessor :audit_comment
 
-        has_many :audits, -> { order(version: :asc) }, as: :auditable, class_name: Audited.audit_class.name
+        has_many :audits, -> { order(id: :asc) }, as: :auditable, class_name: Audit.name
         Audited.audit_class.audited_class_names << to_s
 
         on = Array(options[:on])
